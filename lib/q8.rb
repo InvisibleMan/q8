d75231a5d9ae30f2136f0548aad4df7e8734b9c2
DLarionov <fairness@mail.ru>
2016-04-08 00:58:26 +0300

    Алгорит разделен на классы

--- lib/q8.rb
+++ lib/q8.rb
@@ -3,15 +3,179 @@ module Q8
   EMPTY_CELL = '.'
   QFILL_CELL = 'Q'
   MAX_INDEX = BOARD_SIZE - 1
+
+
+  class Board
+    attr_reader :board
+
+    def initialize
+      @board = []
+      Q8::BOARD_SIZE.times do
+        @board << [Q8::EMPTY_CELL] * Q8::BOARD_SIZE
+      end
+    end
+
+    def size
+      @board.size
+    end
+
+    def rows
+      @board
+    end
+
+    def to_s
+      letters = 'abcdefgh'.chars
+      qq = {}
+
+      @board.each_with_index.map do |r, i|
+        q = r.index(Q8::QFILL_CELL)
+        format("%s%d", letters[q], Q8::BOARD_SIZE - i)
+      end.sort.join(',')
+    end
+
+    def self.from_string(str)
+      brd = Board.new
+
+      letters = 'abcdefgh'.chars
+      qs = []
+
+      str.gsub(' ', '').split(',').each do |pair|
+        qs[Q8::BOARD_SIZE - pair[1].to_i] = letters.index(pair[0])
+      end
+
+      qs.each_with_index do |c, r|
+        brd.board[r][c] = Q8::QFILL_CELL
+      end
+
+      brd
+    end
+
+    def ==(rhs)
+      @board == rhs.board
+    end
+    
+    def valid?(r, c)
+
+      for ci in c.times
+        return false if @board[r][ci] == Q8::QFILL_CELL
+      end
+
+      for ri in r.times
+        return false if @board[ri][c] == Q8::QFILL_CELL
+      end
+
+      ri = r - 1
+      ci = c - 1
+      while (ri > -1 && ci > -1)
+        return false if @board[ri][ci] == Q8::QFILL_CELL
+        ri -= 1
+        ci -= 1
+      end
+
+      ri = r - 1
+      ci = c + 1
+      while (ri > -1 && ci < Q8::BOARD_SIZE)
+        return false if @board[ri][ci] == Q8::QFILL_CELL
+        ri -= 1
+        ci += 1
+      end
+
+      true
+    end
+
+    def cell(r, c, val)
+      @board[r][c] = val
+    end
+
+    def queen(r, c)
+      cell(r, c, Q8::QFILL_CELL)
+    end
+
+    def empty(r, c)
+      cell(r, c, Q8::EMPTY_CELL)
+    end 
+
+    def clone
+      brd = Board.new
+
+      brd.size.times do |r|
+        brd.size.times do |c|
+          brd.cell(r, c, @board[r][c])
+        end
+      end
+
+      brd
+    end
+
+    def queen_index(r)
+      @board[r].index(Q8::QFILL_CELL)
+    end
+
+  end
+
+  class Solver
+    def fill_row_down(r, brd)
+      brd.size.times do |c|
+        if brd.valid?(r, c)
+          brd.queen(r, c)
+
+          return true if r == brd.size - 1
+          return true if fill_row_down(r + 1, brd)
+
+          brd.empty(r, c)
+        end
+      end
+      false
+    end
+
+    def find_first
+      brd = Q8::Board.new
+      fill_row_down(0, brd)
+      brd
+    end
+
+    def fill_row_up(r, brd)
+      return false if r < 0
+
+      q = brd.queen_index(r)
+      brd.empty(r,q)
+
+      for c in (q+1..brd.size - 1)
+        if brd.valid?(r, c)
+          brd.queen(r,c)
+
+          return true if r == brd.size - 1
+          return true if fill_row_down(r + 1, brd)
+          brd.empty(r,c)
+        end
+      end
+
+      return false if r == 0
+      return fill_row_up(r - 1, brd)
+    end
+
+    def find_all
+      brd = find_first
+      i = 0
+      begin
+        yield brd.clone
+        i += 1
+      end while(fill_row_up(Q8::MAX_INDEX, brd))
+      i
+    end
+
+  end
 end
 
-def new_board
-  board = []
-  Q8::BOARD_SIZE.times do
-    board << [Q8::EMPTY_CELL] * Q8::BOARD_SIZE
-  end
-  board
-end
+# Q8::Solver.new.find_first
+
+# Q8::Solver.new.find_each do |solution|
+# end
+
+# brd = Board.new
+# brd.to_s
+
+
 
 def debug_board(board)
   puts "==== BOARD ==="
@@ -19,148 +183,3 @@ def debug_board(board)
     puts board[r].join(' ')
   end
 end
-
-def board_to_s(board)
-  letters = 'abcdefgh'.chars
-  qq = {}
-
-  board.each_with_index.map do |r, i|
-    q = r.index(Q8::QFILL_CELL)
-    format("%s%d", letters[q], Q8::BOARD_SIZE - i)
-  end.sort.join(',')
-end
-
-def board_from_s(str)
-  board = new_board
-
-  letters = 'abcdefgh'.chars
-  qs = []
-
-  str.gsub(' ', '').split(',').each do |pair|
-    qs[Q8::BOARD_SIZE - pair[1].to_i] = letters.index(pair[0])
-  end
-
-  qs.each_with_index do |c, r|
-    board[r][c] = Q8::QFILL_CELL
-  end
-
-  board
-end
-
-def valid_board?(board, r, c)
-
-  for ci in c.times
-    return false if board[r][ci] == Q8::QFILL_CELL
-  end
-
-  for ri in r.times
-    return false if board[ri][c] == Q8::QFILL_CELL
-  end
-
-  ri = r - 1
-  ci = c - 1
-  while (ri > -1 && ci > -1)
-    return false if board[ri][ci] == Q8::QFILL_CELL
-    ri -= 1
-    ci -= 1
-  end
-
-  ri = r - 1
-  ci = c + 1
-  while (ri > -1 && ci < Q8::BOARD_SIZE)
-    return false if board[ri][ci] == Q8::QFILL_CELL
-    ri -= 1
-    ci += 1
-  end
-
-  true
-end
-
-def find_solution
-  board = new_board()
-
-  r = 0
-  c = 0
-
-  while(true)
-    if (r == Q8::BOARD_SIZE)
-      return board
-    end
-
-    if(c == Q8::BOARD_SIZE)
-      unless board[r].index(Q8::QFILL_CELL)
-        r -= 1
-        c = board[r].index(Q8::QFILL_CELL)
-        board[r][c] = Q8::EMPTY_CELL
-        c +=1
-      else
-        c = 0
-        r +=1
-      end
-      next
-    end
-
-    if(valid_board?(board, r, c))
-      board[r][c] = Q8::QFILL_CELL
-      r+=1
-      c = 0
-    else
-      c += 1
-    end
-  end
-
-  return board
-end
-
-def fill_row(r, board)
-  Q8::BOARD_SIZE.times do |c|
-    # puts "fill_row r: #{r}, c: #{c}"
-    if valid_board?(board, r, c)
-      board[r][c] = Q8::QFILL_CELL
-
-      return true if r == Q8::MAX_INDEX
-      return true if fill_row(r + 1, board)
-
-      board[r][c] = Q8::EMPTY_CELL
-    end
-  end
-  return false
-end
-
-
-def fill_row_up(r, board)
-  return false if r < 0
-
-  q = board[r].index(Q8::QFILL_CELL)
-  board[r][q] = Q8::EMPTY_CELL
-
-  for c in (q+1..Q8::MAX_INDEX)
-    if valid_board?(board, r, c)
-      board[r][c] = Q8::QFILL_CELL
-
-      return true if r == Q8::MAX_INDEX
-      return true if fill_row(r + 1, board)
-      board[r][c] = Q8::EMPTY_CELL
-    end
-  end
-
-  return false if r == 0
-  return fill_row_up(r - 1, board)
-end
-
-def find_solution_rec
-  board = new_board()
-  fill_row(0, board)
-  board
-end
-
-def each_solution
-  board = find_solution
-  i = 0
-  begin
-    yield board.clone
-    i += 1
-  end while(fill_row_up(Q8::MAX_INDEX, board))
-  i
-end
-
--- test/q8_test.rb
+++ test/q8_test.rb
@@ -1,33 +1,18 @@
 require 'test_helper'
 require File.expand_path("./lib/q8.rb")
 
-class TestMeme < MiniTest::Unit::TestCase
+class TestQ8 < MiniTest::Unit::TestCase
   def setup
-  end
-
-  def test_new_board
-    board = new_board()
-    assert_equal board.size, Q8::BOARD_SIZE
-    assert_equal board.first.size, Q8::BOARD_SIZE
-  end
-
-  def test_valid_board_2
-    board = new_board()
-    board[0][0] = Q8::QFILL_CELL
-    board[7][7] = Q8::QFILL_CELL
-    assert_equal valid_board?(board, 7, 7), false
+    @test_brd = create_other_solution
   end
 
   def create_first_solution
-    board = new_board()
-    board[0][0] = board[1][4] = Q8::QFILL_CELL
-    board[2][7] = board[3][5] = Q8::QFILL_CELL
-    board[4][2] = board[5][6] = Q8::QFILL_CELL
-    board[6][1] = board[7][3] = Q8::QFILL_CELL
-    board
+    Q8::Board.from_string('a8,b2,c4,d1,e7,f5,g3,h6')
   end
 
   def create_other_solution
+    brd = Q8::Board.new
+
     #   a b c d e f g h
     # 8 . . . Q . . . .
     # 7 . . . . . . Q .
@@ -38,57 +23,73 @@ class TestMeme < MiniTest::Unit::TestCase
     # 2 Q . . . . . . .
     # 1 . . . . . Q . .
 
-    board = new_board()
-    board[0][3] = board[1][6] = Q8::QFILL_CELL
-    board[2][2] = board[3][7] = Q8::QFILL_CELL
-    board[4][1] = board[5][4] = Q8::QFILL_CELL
-    board[6][0] = board[7][5] = Q8::QFILL_CELL
-    board
+    brd.rows[0][3] = brd.rows[1][6] = Q8::QFILL_CELL
+    brd.rows[2][2] = brd.rows[3][7] = Q8::QFILL_CELL
+    brd.rows[4][1] = brd.rows[5][4] = Q8::QFILL_CELL
+    brd.rows[6][0] = brd.rows[7][5] = Q8::QFILL_CELL
+    brd
   end
 
-  def create_third_solution
-    board_from_s('a2,b4,c6,d8,e3,f1,g7,h5')
-  end
 
-  # def test_find_solution_rec
-  #   skip "test this later"
-  #   solution = create_first_solution
-  #   res = find_solution_rec
-  #   res = find_solution
-  #   puts "\nRESULT"
-  #   # debug_board(res)
-  #   # debug_board(solution)
-
-  #   assert_equal solution, res
+  # def create_third_solution
+  #   board_from_s('a2,b4,c6,d8,e3,f1,g7,h5')
   # end
 
-  def test_each_solution
-    # skip "test this later"
-    i = 0
-    count = each_solution do |s|
-      i += 1
-    end
-    assert_equal 92, count
+  def test_new_board
+    brd = Q8::Board.new
+
+    assert_equal brd.size, Q8::BOARD_SIZE
+    assert_equal brd.rows.first.size, Q8::BOARD_SIZE
+  end
+
+  def test_clone
+    brd = Q8::Board.new
+    brd.queen(5, 5)
+
+    brd_clone = brd.clone
+
+    assert_equal brd, brd_clone
+    assert_equal brd.board, brd_clone.board
+    assert brd.object_id != brd_clone.object_id
   end
 
   def test_board_to_s
-    board = create_other_solution
+    brd = create_other_solution
 
-    assert_equal 'a2,b4,c6,d8,e3,f1,g7,h5', board_to_s(board)
+    assert_equal 'a2,b4,c6,d8,e3,f1,g7,h5', brd.to_s
   end
 
   def test_board_from_s
-    board = create_other_solution
+    brd = create_other_solution
 
-    assert_equal board_from_s('a2,b4,c6,d8,e3,f1,g7,h5'), board
-    assert_equal board_from_s('a2, b4, c6, d8, e3, f1, g7, h5'), board
+    assert_equal Q8::Board.from_string('a2,b4,c6,d8,e3,f1,g7,h5'), brd
+    assert_equal Q8::Board.from_string('a2, b4, c6, d8, e3, f1, g7, h5'), brd
   end
 
-  def test_find_new_solution
-    board = create_third_solution
+  def test_board_valid?
+    brd = Q8::Board.new
+    brd.queen(0, 0)
 
-    next_board = fill_row_up(Q8::MAX_INDEX, board)
+    assert_equal brd.valid?(7, 7), false
+    assert_equal brd.valid?(0, 7), false
+    assert_equal brd.valid?(7, 0), false
+    assert_equal brd.valid?(2, 3), true
+    assert_equal brd.valid?(3, 2), true
+  end
 
-    assert_equal true, next_board
+  def test_find_solution_rec
+    solution = create_first_solution
+    res =  Q8::Solver.new.find_first
+
+    assert_equal solution, res
+  end
+
+  def test_each_solution
+    i = 0
+    count = Q8::Solver.new.find_all do |s|
+      i += 1
+    end
+
+    assert_equal 92, count
   end
 end
